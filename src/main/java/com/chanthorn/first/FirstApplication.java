a160dd4c8f5b759ac9e31da71d07b2dc6c24e90e
chanthornsp <hoeurnchanthorn@gmail.com>
2025-10-31 17:01:15 +0700

    first init

---
+++ src/main/java/com/chanthorn/first/FirstApplication.java
@@ -0,0 +1,16 @@
+package com.chanthorn.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class FirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstApplication.class, args);
+    }
+
+}
